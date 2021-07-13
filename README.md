584c8ee7c4b98626765bfb6f65d5a24c384e883e
ivanprytula <ivanprytula87@gmail.com>
2021-07-13 18:18:29 +0300

    Integrate TG bot with celery beat task

--- README.md
+++ README.md
@@ -4,7 +4,26 @@
 Newly published articles/posts from Django blog to Telegram channel.
 
 ## When?
-Every defined period of time (10min) run task/-s with django-celery-beat.
+Every defined period of time (x min) run task/-s with django-celery-beat.
 
 ## Why?
-Learn how to work with Celery and Telegram API.
+Learn how to work with Celery/Celery Beat and Telegram API.
+
+
+"Modus operandi":
+1. Create task with @shared_task decorator for app logic in <app_name>/tasks.py
+2. Create celery-beat Periodic Tasks in Django admin page.
+3. Run Redis server (in Docker or system-wide): 
+```shell
+docker run -p 6379:6379 --name some-redis -d redis
+```
+4. Run Celery worker from project root (where manage.py is located): 
+```shell
+celery -A core_config worker -l INFO
+```
+5. Run Celery Beat with scheduler:
+```shell
+celery -A core_config beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
+```
+
+Bot's username: DjCeleryPosts_Bot
--- dj_posts_to_telegram/core_config/settings.py
+++ dj_posts_to_telegram/core_config/settings.py
@@ -33,56 +33,79 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth',
-                  'django.contrib.contenttypes', 'django.contrib.sessions',
-                  'django.contrib.messages', 'django.contrib.staticfiles',
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
-                  'blog', 'django_celery_beat', ]
+    'blog',
+    'django_celery_beat',
+]
 
-MIDDLEWARE = ['django.middleware.security.SecurityMiddleware',
-              'django.contrib.sessions.middleware.SessionMiddleware',
-              'django.middleware.common.CommonMiddleware',
-              'django.middleware.csrf.CsrfViewMiddleware',
-              'django.contrib.auth.middleware.AuthenticationMiddleware',
-              'django.contrib.messages.middleware.MessageMiddleware',
-              'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = 'core_config.urls'
 
 TEMPLATES = [
-    {'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [],
-     'APP_DIRS': True, 'OPTIONS': {
-        'context_processors': ['django.template.context_processors.debug',
-                               'django.template.context_processors.request',
-                               'django.contrib.auth.context_processors.auth',
-                               'django.contrib.messages.context_processors.messages', ], }, }, ]
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors':
+                [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'core_config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3',
-                         'NAME': BASE_DIR / 'db.sqlite3', }}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [{'NAME': 'django.contrib.auth.password_validation'
-                                     '.UserAttributeSimilarityValidator', },
-                            {'NAME': 'django.contrib.auth.password_validation'
-                                     '.MinimumLengthValidator', },
-                            {'NAME': 'django.contrib.auth.password_validation'
-                                     '.CommonPasswordValidator', },
-                            {'NAME': 'django.contrib.auth.password_validation'
-                                     '.NumericPasswordValidator', }, ]
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation'
+             '.UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation'
+             '.MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation'
+             '.CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation'
+             '.NumericPasswordValidator', },
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -103,3 +126,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Celery Configuration Options
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+
+TELEGRAM_API = {
+    'bot_token': '1772567719:AAGrOu0Bl-hWpn9VQa-GmeUAbgSNdkaLe7A',
+    'channel_name': 'djcelery',
+}
